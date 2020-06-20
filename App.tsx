5ba1293dc8095bc628c116841ffb855702cf985e
Roman Mahotskyi <roman.mahotskyi@gmail.com>
2020-06-20 17:33:59 +0300

    Add initial project to reproduce issue with react-native-screens

--- App.tsx
+++ App.tsx
@@ -1,19 +1,129 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, TouchableOpacity, View, ViewStyle, StyleProp } from 'react-native';
+import { enableScreens } from 'react-native-screens';
+import { createNativeStackNavigator } from 'react-native-screens/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useSafeArea, SafeAreaProvider } from 'react-native-safe-area-context';
+
+enableScreens();
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+const NestedStack = createNativeStackNavigator();
+
+const buttonStyle: StyleProp<ViewStyle> = {
+  margin: 20,
+  backgroundColor: 'pink',
+  padding: 10,
+  borderRadius: 10,
+  display: 'flex'
+};
+
+function Tab1(props: any) {
+  const insets = useSafeArea();
+
+  return (
+    <View style={{ marginTop: insets.top }}>
+      <Text>I am Tab1 (Please, go to Tab2)</Text>
+    </View>
+  )
+}
+
+function Tab2() {
+  return (
+    <NestedStack.Navigator initialRouteName='Screen4' >
+      <NestedStack.Screen name='Screen4' options={{ headerShown: false }} component={Screen4} />
+      <NestedStack.Screen name='Screen5' component={Screen5} />
+    </NestedStack.Navigator>
+  )
+}
+
+function Screen1() {
+  return (
+    <Tab.Navigator initialRouteName='Tab1'>
+      <Tab.Screen name='Tab1' component={Tab1} />
+      <Tab.Screen name='Tab2' component={Tab2} />
+    </Tab.Navigator>
+  )
+}
+
+
+function Screen2(props: any) {
+  const insets = useSafeArea();
+
+  return (
+    <View style={{ marginTop: insets.top }}>
+      <TouchableOpacity style={buttonStyle} onPress={() => props.navigation.push('Screen3')}>
+        <Text>I am Screen2, Navigate to Screen3</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+function Screen3(props: any) {
+  const insets = useSafeArea();
+
+  // Issue arise after calling this function
+  const handleNavigation = () => {
+    props.navigation.navigate('Screen1', {
+      screen: 'Tab2',
+      initial: false,
+      params: {
+        screen: 'Screen5',
+        initial: false,
+      },
+    });
+  }
+
+  return (
+    <View style={{ marginTop: insets.top }}>
+      <TouchableOpacity style={buttonStyle} onPress={handleNavigation}>
+        <Text>I am Screen3, Navigate to Screen 5</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+function Screen4(props: any) {
+  const insets = useSafeArea();
+
+  return (
+    <View style={{ marginTop: insets.top }}>
+      {/* Click this button first */}
+      <TouchableOpacity style={buttonStyle} onPress={() => props.navigation.navigate('Screen2')}>
+        <Text>I am Screen4, Navigate to Screen2</Text>
+      </TouchableOpacity>
+      {/* Click this button at the end of the flow */}
+      <TouchableOpacity style={buttonStyle} onPress={() => props.navigation.navigate('Screen5')}>
+        <Text>I am Screen4, Navigate to Screen5 (click it after reproducing issue)</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+// This screens cannot be opened properly
+function Screen5() {
+  const insets = useSafeArea();
+
+  return (
+    <View style={{ marginTop: insets.top }}>
+      <Text>I am Screen5</Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Screen1' screenOptions={{ headerShown: false }}>
+          <Stack.Screen name='Screen1' component={Screen1} />
+          <Stack.Screen name='Screen2' component={Screen2} />
+          <Stack.Screen name='Screen3' component={Screen3} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
